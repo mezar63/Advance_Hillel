08f95be1630cfbf0952f767578de515d86af12cd
mezar63 <Trubchaninmax@gmail.com>
2023-09-14 18:18:47 +0200

    parse_cookie

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-def parse(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+# def parse(query: str) -> dict:
+#     return {}
+#
+#
+# if __name__ == '__main__':
+#     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+#     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+#     assert parse('http://example.com/') == {}
+#     assert parse('http://example.com/?') == {}
+#     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    cookie_dict = {}
+
+    if not query:
+        return cookie_dict  # Return an empty dictionary if the input is empty
+
+    pairs = query.split(';')
+
+    for pair in pairs:
+        key_value = pair.strip().split('=', 1)
+
+        if len(key_value) == 2:
+            key, value = key_value
+            cookie_dict[key] = value
+
+    return cookie_dict
 
 
 if __name__ == '__main__':
